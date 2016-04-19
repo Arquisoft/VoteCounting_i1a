b959a41c5e3f478052b2ad30064b2e29f97e142a
Sergio <sergioifg94@gmail.com>
2016-04-19 20:08:24 +0200

    Forgot to use Results class in the rest controller

--- src/main/java/es/uniovi/asw/controller/RestService.java
+++ src/main/java/es/uniovi/asw/controller/RestService.java
@@ -2,6 +2,7 @@ package es.uniovi.asw.controller;
 
 import es.uniovi.asw.model.VoteCount;
 import es.uniovi.asw.persistence.VoteCountRepository;
+import es.uniovi.asw.results.Results;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class RestService {
 
     @RequestMapping("/api/results")
     public Iterable<VoteCount> results() {
-        return voteCountRepository.findAll();
+        return new Results(voteCountRepository).results();
     }
 
 }
